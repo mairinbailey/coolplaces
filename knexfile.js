ca63f3a3818c557b77625d002089bbf14b84cfcc
Mairin Bailey <mairin.bailey@gmail.com>
2016-06-16 11:04:01 -0600

    initial commit

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+require('dotenv').load();
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/coolplaces'
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL + '?ssl=true'
+  }
+};
